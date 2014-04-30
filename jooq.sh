5ff4fa5f8ecfbbfcb8d3f01a4b98d5e3eee3fca9
linux_china <linux_china@hotmail.com>
2014-04-30 09:21:20 +0800

    add generate logic

--- jooq.sh
+++ jooq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn dependency:resolve
-
-
+##mvn dependency:resolve
+M2_REPO=~/.m2/repository
+java -classpath "${M2_REPO}/org/jooq/jooq/3.3.1/jooq-3.3.1.jar:${M2_REPO}/org/jooq/jooq-meta/3.3.1/jooq-meta-3.3.1.jar:${M2_REPO}/org/jooq/jooq-codegen/3.3.1/jooq-codegen-3.3.1.jar:${M2_REPO}/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar:." org.jooq.util.GenerationTool /jooq.xml
