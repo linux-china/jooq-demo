a5960982e7f107c50622dac25a4ddf902110dd53
linux_china <linux_china@hotmail.com>
2015-05-06 17:50:10 +0800

    change jooq version

--- jooq.sh
+++ jooq.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+JOOQ_VERSION="3.6.1"
 ##mvn dependency:resolve
 M2_REPO=~/.m2/repository
-java -classpath "${M2_REPO}/org/jooq/jooq/3.3.1/jooq-3.3.1.jar:${M2_REPO}/org/jooq/jooq-meta/3.3.1/jooq-meta-3.3.1.jar:${M2_REPO}/org/jooq/jooq-codegen/3.3.1/jooq-codegen-3.3.1.jar:${M2_REPO}/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar:." org.jooq.util.GenerationTool /jooq.xml
+java -classpath "${M2_REPO}/org/jooq/jooq/${JOOQ_VERSION}/jooq-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-meta/${JOOQ_VERSION}/jooq-meta-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-codegen/${JOOQ_VERSION}/jooq-codegen-${JOOQ_VERSION}.jar:${M2_REPO}/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar:." org.jooq.util.GenerationTool /jooq.xml
