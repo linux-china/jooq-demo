70dede2b5c3f340b98d1888b2203c6ea547d9355
linux_china <linux_china@hotmail.com>
2015-05-07 10:07:53 +0800

    add suffix

--- jooq.sh
+++ jooq.sh
@@ -3,4 +3,4 @@
 JOOQ_VERSION="3.6.1"
 ##mvn dependency:resolve
 M2_REPO=~/.m2/repository
-java -classpath "${M2_REPO}/org/jooq/jooq/${JOOQ_VERSION}/jooq-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-meta/${JOOQ_VERSION}/jooq-meta-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-codegen/${JOOQ_VERSION}/jooq-codegen-${JOOQ_VERSION}.jar:${M2_REPO}/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar:." org.jooq.util.GenerationTool /jooq.xml
+java -Dorg.jooq.util.example.java-class-name-suffix=Table -classpath "${M2_REPO}/org/jooq/jooq/${JOOQ_VERSION}/jooq-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-meta/${JOOQ_VERSION}/jooq-meta-${JOOQ_VERSION}.jar:${M2_REPO}/org/jooq/jooq-codegen/${JOOQ_VERSION}/jooq-codegen-${JOOQ_VERSION}.jar:${M2_REPO}/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar:." org.jooq.util.GenerationTool /jooq.xml
