ed17b09dd5cc238646ca8352ba863bd9da57c984
linux_china <linux_china@hotmail.com>
2015-05-07 10:12:59 +0800

    language repository

---
+++ src/main/java/org/mvnsearch/domain/model/language/LanguageRepository.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.domain.model.language;
+
+import org.mvnsearch.domain.shared.CrudRepository;
+
+/**
+ * language repository
+ *
+ * @author linux_china
+ */
+public interface LanguageRepository extends CrudRepository<Language, Integer> {
+}
