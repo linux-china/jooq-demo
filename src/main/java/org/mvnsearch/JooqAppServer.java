e2e69ea65453c8e856d69149a4d38faa4de178bf
linux_china <libing.chen@gmail.com>
2020-08-04 12:44:27 -0700

    JooqAppServer

---
+++ src/main/java/org/mvnsearch/JooqAppServer.java
@@ -0,0 +1,11 @@
+package org.mvnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqAppServer {
+    public static void main(String[] args) {
+        SpringApplication.run(JooqAppServer.class, args);
+    }
+}
