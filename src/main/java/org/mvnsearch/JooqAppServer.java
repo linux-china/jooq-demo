4d1f044202a4d62b83975d154822813fbfd96811
linux_china <libing.chen@gmail.com>
2021-07-07 15:00:58 -0700

    Polishing

--- src/main/java/org/mvnsearch/JooqAppServer.java
+++ src/main/java/org/mvnsearch/JooqAppServer.java
@@ -2,8 +2,9 @@ package org.mvnsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
 public class JooqAppServer {
     public static void main(String[] args) {
         SpringApplication.run(JooqAppServer.class, args);
