eb50d488f340841aab5c05516e04961aa474456a
linux_china <libing.chen@gmail.com>
2021-11-23 10:44:21 -0800

    Add @Dbunit

--- src/test/java/org/mvnsearch/JooqBaseTest.java
+++ src/test/java/org/mvnsearch/JooqBaseTest.java
@@ -1,5 +1,6 @@
 package org.mvnsearch;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.spring.api.DBRider;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -7,5 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(classes = JooqAppServer.class)
 @ActiveProfiles("test")
 @DBRider
+@DBUnit(schema = "jooq")
 public abstract class JooqBaseTest {
 }
