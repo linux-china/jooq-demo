757ef4cda8e6256464768db143048e76b1e5b9cc
Libing Chen <libing.chen@gmail.com>
2024-11-06 14:57:47 +0800

    chore: add dataset

--- src/test/java/org/mvnsearch/domain/model/language/LanguageRepositoryTest.java
+++ src/test/java/org/mvnsearch/domain/model/language/LanguageRepositoryTest.java
@@ -1,10 +1,12 @@
 package org.mvnsearch.domain.model.language;
 
+import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.JooqBaseTest;
 import org.mvnsearch.infrastructure.jooq.tables.records.LanguageRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@DataSet({"/database/dataset/languages.xml"})
 public class LanguageRepositoryTest extends JooqBaseTest {
 
   @Autowired
