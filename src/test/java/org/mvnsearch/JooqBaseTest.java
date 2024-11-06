129e019f9befb713c99484c9583ed152a0551a9b
Libing Chen <libing.chen@gmail.com>
2024-11-06 14:56:25 +0800

    chore: add disableSequenceFiltering

--- src/test/java/org/mvnsearch/JooqBaseTest.java
+++ src/test/java/org/mvnsearch/JooqBaseTest.java
@@ -1,6 +1,7 @@
 package org.mvnsearch;
 
 import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.connection.RiderDataSource;
 import com.github.database.rider.spring.api.DBRider;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,6 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(classes = JooqAppServer.class)
 @ActiveProfiles("test")
 @DBRider
-@DBUnit(schema = "jooq")
+@DBUnit(schema = "jooq", disableSequenceFiltering = true, expectedDbType = RiderDataSource.DBType.MYSQL)
 public abstract class JooqBaseTest {
 }
