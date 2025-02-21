103f0b82672a9d53c02ada34a90ea9cb48db73bd
Libing Chen <libing.chen@gmail.com>
2025-02-21 13:07:44 +0800

    chore: polishing

--- src/test/java/org/mvnsearch/domain/model/language/LanguageRepositoryTest.java
+++ src/test/java/org/mvnsearch/domain/model/language/LanguageRepositoryTest.java
@@ -6,6 +6,8 @@ import org.mvnsearch.JooqBaseTest;
 import org.mvnsearch.infrastructure.jooq.tables.records.LanguageRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataSet({"/database/dataset/languages.xml"})
 public class LanguageRepositoryTest extends JooqBaseTest {
 
@@ -14,7 +16,8 @@ public class LanguageRepositoryTest extends JooqBaseTest {
 
   @Test
   public void testDao() {
-    final LanguageRecord language = languageRepository.findById(1).get();
+    final LanguageRecord language = languageRepository.findById(1);
+    assertThat(language).isNotNull();
     System.out.println(language.getId());
   }
 }
