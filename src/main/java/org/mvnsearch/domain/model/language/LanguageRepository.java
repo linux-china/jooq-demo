f6cb2ef8b88940d00074020db1bfd1b5ca432080
Libing Chen <libing.chen@gmail.com>
2025-02-21 12:25:02 +0800

    chore: polishing

--- src/main/java/org/mvnsearch/domain/model/language/LanguageRepository.java
+++ src/main/java/org/mvnsearch/domain/model/language/LanguageRepository.java
@@ -1,7 +1,7 @@
 package org.mvnsearch.domain.model.language;
 
 import org.jooq.DSLContext;
-import org.mvnsearch.infrastructure.jooq.AbstractSpringDAOImpl;
+import org.mvnsearch.domain.model.BaseSpringJooqDAO;
 import org.mvnsearch.infrastructure.jooq.tables.Language;
 import org.mvnsearch.infrastructure.jooq.tables.records.LanguageRecord;
 import org.springframework.stereotype.Component;
@@ -12,17 +12,10 @@ import org.springframework.stereotype.Component;
  * @author linux_china
  */
 @Component
-public class LanguageRepository extends AbstractSpringDAOImpl<LanguageRecord, LanguageRecord, Integer> {
-  private final DSLContext dslContext;
+public class LanguageRepository extends BaseSpringJooqDAO<LanguageRecord, Integer> {
 
   public LanguageRepository(DSLContext dslContext) {
-    super(Language.LANGUAGE, LanguageRecord.class, dslContext.configuration());
-    this.dslContext = dslContext;
-  }
-
-  @Override
-  public DSLContext ctx() {
-    return this.dslContext;
+    super(dslContext, Language.LANGUAGE, LanguageRecord.class);
   }
 
   @Override
