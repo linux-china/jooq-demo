a8683a95512f12dc6b3139200d499f18ce46a012
Libing Chen <libing.chen@gmail.com>
2025-04-20 15:16:31 +0800

    chore: polishing

--- src/test/java/org/mvnsearch/jooq/JooqDemoTest.java
+++ src/test/java/org/mvnsearch/jooq/JooqDemoTest.java
@@ -1,11 +1,11 @@
 package org.mvnsearch.jooq;
 
 import com.github.database.rider.core.api.dataset.DataSet;
-import org.jooq.Record;
 import org.jooq.Result;
 import org.jooq.impl.DefaultDSLContext;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.JooqBaseTest;
+import org.mvnsearch.infrastructure.jooq.tables.records.LanguageRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Flux;
 
@@ -20,21 +20,23 @@ import static org.mvnsearch.infrastructure.jooq.Tables.LANGUAGE;
  */
 @DataSet({"/database/dataset/languages.xml"})
 public class JooqDemoTest extends JooqBaseTest {
-    @Autowired
-    private DefaultDSLContext jooq;
+  @Autowired
+  private DefaultDSLContext jooq;
 
-    @Test
-    public void testSpike() throws Exception {
-        Result<Record> result = jooq.select().from(LANGUAGE).fetch();
-        result.forEach(record -> System.out.println(record.getValue(LANGUAGE.ID)));
-    }
+  @Test
+  public void testSpike() throws Exception {
+    Result<LanguageRecord> result = jooq.selectFrom(LANGUAGE).fetch();
+    result.forEach(language -> {
+      System.out.println(language.getId());
+    });
+  }
 
-    @Test
-    public void testReactive() {
-        final List<String> titles = Flux.from(jooq.select(LANGUAGE.DESCRIPTION).from(LANGUAGE))
-                .map(r -> r.get(LANGUAGE.DESCRIPTION))
-                .collectList()
-                .block();
-        System.out.println(titles);
-    }
+  @Test
+  public void testReactive() {
+    final List<String> titles = Flux.from(jooq.select(LANGUAGE.DESCRIPTION).from(LANGUAGE))
+      .map(r -> r.get(LANGUAGE.DESCRIPTION))
+      .collectList()
+      .block();
+    System.out.println(titles);
+  }
 }
